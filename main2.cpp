8bdc7e00e62695817f3c87539078a0eb04e4cb35
Timofii0507 <timofiiyurkov@gamil.com>
2023-11-30 19:23:40 +0200

    Друге  завдання  з  домашньої  роботи

--- main2.cpp
+++ main2.cpp
@@ -2,7 +2,7 @@
 #include <Windows.h>
 using namespace std;
 
-int main() 
+int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
@@ -10,21 +10,18 @@ int main()
     const int row = 5;
     int arr[col][row];
     int sum_rows[col]{0,0,0,0,0};
-    int sum_cols[row]{0,0,0,0.0};
+    int sum_cols[row]{0,0,0,0,0};
     int firstElement;
-    cout << "Введіть  перше  значенння: ";
+    cout << "Введіть перше значення: ";
     cin >> firstElement;
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            if (j == 0)
-                arr[i][j] = firstElement;
-            else
-                arr[i][j] = arr[i][j - 1] * 2;
+            arr[i][j] = firstElement + i + j;
         }
     }
-    cout << "Створений масив" << endl;
+    cout << "Створений масив: " << endl;
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
